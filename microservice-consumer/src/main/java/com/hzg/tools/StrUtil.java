de744ad134490134275f905125b1d79a46dc8c0f
smjie2800 <sunminjie2800@163.com>
2017-08-25 10:02:30 +0800

    add date perfix to random string

--- microservice-consumer/src/main/java/com/hzg/tools/StrUtil.java
+++ microservice-consumer/src/main/java/com/hzg/tools/StrUtil.java
@@ -6,7 +6,23 @@ import java.util.Random;
 
 @Component
 public class StrUtil {
+    private char[] chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    private char[] numbers = "0123456789".toCharArray();
+
+
     public String generateRandomStr(int strLength) {
+        return generateStr(strLength, chars);
+    }
+
+    public String generateDateRandomStr(int strLength) {
+        return System.currentTimeMillis() + generateRandomStr(strLength);
+    }
+
+    public String generateRandomNumberStr(int strLength) {
+        return generateStr(strLength, numbers);
+    }
+
+    public String generateStr(int strLength, char[] chars) {
         String randomStr = null;
 
         if (strLength > 0) {
@@ -14,9 +30,9 @@ public class StrUtil {
         }
 
         Random random = new Random();
-        char [] str="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
         for ( int i = 0 ; i < strLength ; i++) {
-            randomStr += String.valueOf(str[random.nextInt(62)]);
+            randomStr += String.valueOf(chars[random.nextInt(chars.length)]);
         }
 
         return randomStr;
